aa615f0dd9723c2a8de6e267ce5128908d55355e
Mike Ulvila <mikeulvila@me.com>
2016-02-17 21:33:06 -0600

    portfolio total value working

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,5 +1,6 @@
 'use strict'
 const request = require('request');
+const _ = require('lodash');
 
 // require model
 const Stock = require('../models/stock.js');
@@ -9,9 +10,15 @@ module.exports.index = (req, res) => {
   Stock.getStocks((err, stocks) => {
     if (err) throw err;
 
+    // lodash .reduce for total stock value
+    const totalStockValue = _.round(_.reduce(stocks, (sum, stock) => {
+      return sum + (stock.price * stock.qty);
+    }, 0), 2);
+
     res.render('portfolio', {
       title: 'Portfolio',
-      stocks: stocks
+      stocks: stocks,
+      total: totalStockValue
     });
   });
 };
